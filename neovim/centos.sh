65803479e1b5a5467f86619f3830286940aed671
yangxingwu <xingwu.yang@gmail.com>
2019-04-15 00:24:56 +0800

    add install script for centos

---
+++ neovim/centos.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# on-my-zsh
+sudo yum install zsh
+sudo chsh $USER --shell /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# dev tools (gcc, git, etc)
+sudo yum group install "Development Tools"
+
+# neovim
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y neovim # python{2,34}-neovim
+
+# neovim configuration
+mkdir -p ~/.config/nvim/
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
